cb1f720a27acaa46f7d2f24e84d3f72ca5139225
MohamedCS <you@example.com>
2022-02-06 23:29:46 +0100

    test bot add time interval 24h change date formats

--- weather_bot.py
+++ weather_bot.py
@@ -1,13 +1,15 @@
+import datetime
+import time
 import requests
 
 url = "https://yahoo-weather5.p.rapidapi.com/weather"
 
-bot_key = "5126869787:AAFp65OYO2VW5FhVH6nTo_9l4p3ONqHAzUE"
+bot_token = "5126869787:AAFp65OYO2VW5FhVH6nTo_9l4p3ONqHAzUE"
 
 chat_id = "952979574"
 
 
-time_ref = 60 * 60 * 24
+time_ref = 60 
 
 def send_data():
 
@@ -20,7 +22,27 @@ def send_data():
 
  response = requests.request("GET", url, headers=headers, params=querystring).json()
  
- t = response['city'][0]
- print(t)
+ while(True):
+  
+  for i in range(0 ,11):
+
+     day = response['forecasts'][i]['day']
+ 
+     low = response['forecasts'][i]['low']
+ 
+     high = response['forecasts'][i]['high']
+ 
+     description = response['forecasts'][i]['text']
+ 
+     date = datetime.datetime.fromtimestamp(response['forecasts'][i]['date']).strftime("%d/%m/%Y")
+  
+     msg = f"date -->{day} {date} \ncountry --> Algeria\ncity --> Alger center \nlow-->{low}\nhigh-->{high}\n--{description}--"
+  
+     print(msg)
+  
+     send_url = f"https://api.telegram.org/bot{bot_token}/sendMessage?chat_id={chat_id}&text={msg}"
+     requests.get(send_url)
+  
+  time.sleep(time_ref)
 
 send_data()
